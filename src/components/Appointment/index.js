a1cf2d614db8b78759ca7cbfc0c192f18ed55b0c
akbartels <akunrau@gmail.com>
2020-05-13 14:38:26 -0700

    Add Empty/Booked stories to Appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import "components/Appointment/styles.scss";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 export default function Appointment(props) {
-
+  console.log("PROPS:", props)
 
   return (
-    <article className="appointment">
-    </article>
+    <>
+      <Header time={props.time}></Header>
+      
+
+       { props.interviewer ? 
+        <Show 
+          student={props.interviewer.student}
+          interviewer={props.interviewer.interviewer.name}
+        /> : 
+        <Empty 
+        
+        /> 
+       }
+      
+    
+      <article className="appointment">
+      </article>
+    </>
   )
 }
